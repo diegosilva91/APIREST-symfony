48d2114a2a42b0e407fee282b140a48ea0fbd9e9
diegosilva91 <diegoarturosilva@hotmail.com>
2020-03-13 02:39:02 +0100

    Create controller and post route for companies

---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CompaniesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class CompanyController
+ * @package App\Controller
+ *
+ * @Route(path="/api/")
+ */
+class CompanyController extends AbstractController
+{
+    private $companiesRepository;
+    public function __construct(CompaniesRepository $companyRepository)
+    {
+        $this->companiesRepository=$companyRepository;
+    }
+
+    /**
+     * @Route("company", name="add_company", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function add(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $name = $data['name'];
+        $unique_code = $data['unique_code'];
+        $description = $data['description'];
+        $logo=$data['logo'];
+
+        if (empty($name) || empty($unique_code)) {
+            throw new NotFoundHttpException('Expecting mandatory parameters!');
+        }
+        $this->companiesRepository->saveCompanies($name, $unique_code, $description,$logo);
+        return new JsonResponse(['status' => 'Company created!'], Response::HTTP_CREATED);
+    }
+//    /**
+//     * @Route("company/{id}", name="get_one_company", methods={"GET"})
+//     */
+//    public function get($id): JsonResponse
+//    {
+//        $company = $this->companiesRepository->findOneBy(['id' => $id]);
+//
+//        $data = [
+//            'id' => $company->getId(),
+//            'name' => $company->getName(),
+//            'unique_code' => $company->getUniqueCode(),
+//            'description' => $company->getDescription(),
+//            'logo' => $company->getLogo(),
+//        ];
+//
+//        return new JsonResponse($data, Response::HTTP_OK);
+//    }
+
+}
